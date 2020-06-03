6b0796a1ceb04dfd67d619357292d8b0975f7b88
Konrad Hyzy <konrad.hyzy@al-enterprise.com>
2020-06-03 16:24:44 +0200

    First commit

---
+++ src/script.js
@@ -0,0 +1,5 @@
+console.log("---\nVanilla Boilerplate\n---");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#333";
+ctx.fillRect(0, 0, 150, 150);
